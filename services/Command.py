69443b9adfd8e464589704b409cd800ad88954ad
Julio Cavalin <julio.cavalin@gmail.com>
2021-11-28 11:55:47 -0300

    feat: Add greeting message

--- services/Command.py
+++ services/Command.py
@@ -1,6 +1,4 @@
 class Command:
-    STATE_WAITING_KEYWORD = 'WAITING_KEYWORD'
-
     def __init__(self):
         self.origin = None
         self.state = None
@@ -8,5 +6,9 @@ class Command:
     def responses(self, text, reply):
         input_text = str(text).lower()
 
-        return 'Hi!'
+        return "Hi! I'm Earll.\n\n" \
+               "I can help you to convert some kinds of messages.\n" \
+               "\t\t\t1. Send me a text and I will speak it to you.\n" \
+               "\t\t\t2. Send me audio and I will transcribe it for you.\n" \
+               "\t\t\t3. Send me a picture and I will describe it to you.\n"
 
